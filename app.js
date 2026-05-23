1ca3ca79ad3a0fe9807de57a21c619c6004dcde7
khushi05sharma <5103.sharmakhushi@gmail.com>
2026-05-23 19:07:41 +0530

    adding experience section

--- app.js
+++ app.js
@@ -31,7 +31,21 @@ form.addEventListener("submit", function (e) {
     });
 });
 
+/* HAMBURGER MENU */
 
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
 
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navLinks.classList.toggle("active");
+});
 
+/* CLOSE MENU WHEN LINK CLICKED */
 
+document.querySelectorAll(".nav-link").forEach((link) => {
+  link.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navLinks.classList.remove("active");
+  });
+});
